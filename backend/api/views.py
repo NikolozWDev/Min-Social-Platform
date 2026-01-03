ecb5b4368110cec1e3d756ab5bff98be97b27e54
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2026-01-03 11:21:05 +0400

    view imports

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics, views, response, serializers
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import CustomUser
+from .serializers import RegisterSerializer, EmailTokenObtainPairSerializer
 
 # Create your views here.
